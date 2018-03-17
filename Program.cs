99f496ec6a3e938277729cfde5cbde23af140ea7
gangdu1997 <gangdu1997@gmail.com>
2018-03-17 16:17:06 +0800

    Hello world
    Hello world.讲解 write 和writeline的不同。writeline 默认 有 \n ，转换到下一行。代码分析，注释上有

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//这里是注释 下面的是引入命名空间
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//定义命名空间 从{开始，到}结束
+namespace ConsoleApplication1
+{
+    //定义类
+    class Program
+    {
+        //定义一个Main方法
+        static void Main(string[] args)
+        {
+            //方法体 
+            Console.WriteLine("第一行");
+            Console.Write("第一行");
+        }
+    }
+}
