a8e84e3ddb1ad1e3c6d044ce070d4ee5f726cf3c
gangdu1997 <gangdu1997@gmail.com>
2018-03-17 17:09:33 +0800

    转义字符，\在引号，内部的作用。\t 向后四格，\\出现一个\，\"出现引号。

---
+++ 3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp = 100;
+            string name = "siki";
+
+            Console.Write("名字：\t  \\  \"{0}\"\n hp：{1}",name,hp);//内部的引号 都加 \，\" 表示 普通字符。
+            Console.ReadKey();
+        }
+    }
+}
