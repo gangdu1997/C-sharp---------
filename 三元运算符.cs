e17439006896f6286448fd7dd47716ea7f32a849
gangdu1997 <gangdu1997@gmail.com>
2018-03-18 16:45:07 +0800

    sco >= 60 ? "成绩及格" : "成绩不及格";

---
+++ 三元运算符.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string score = Console.ReadLine();
+            int sco = Convert.ToInt32(score);
+            string resSco = sco >= 60 ? "成绩及格" : "成绩不及格";
+            Console.WriteLine(resSco);
+            Console.ReadKey();
+        }
+    }
+}
