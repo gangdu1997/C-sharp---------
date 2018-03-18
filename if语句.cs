f461ebb82846dbcf3116904f1b55504d93cbc3fd
gangdu1997 <gangdu1997@gmail.com>
2018-03-18 16:31:42 +0800

    if语句

---
+++ if语句.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool var = false;
+            if(var)
+                Console.WriteLine("---------");
+            Console.WriteLine("if语句后边的语句");
+
+            if (34 <= 40)
+                Console.WriteLine("---------");
+            Console.WriteLine("if语句后边的语句");
+            if (var == false)
+                Console.WriteLine("---------");
+            Console.WriteLine("if语句后边的语句");
+            Console.ReadKey();
+        }
+    }
+}
