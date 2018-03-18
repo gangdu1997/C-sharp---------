bb36a30f852f6bc3406f40017b6708b1dd5b2a79
gangdu1997 <gangdu1997@gmail.com>
2018-03-18 16:23:39 +0800

    goto语句，即 跳转

---
+++ goto语句，即 跳转.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInteger = 5;
+            goto mylabel;//goto语句用来控制程序跳转到某个位置
+            myInteger ++;
+        mylabel: Console.WriteLine(myInteger);
+            Console.ReadKey();
+        }
+    }
+}
