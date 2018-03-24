e8a3e70dc5b2b184ee3375642411a22c7915afcd
gangdu1997 <gangdu1997@gmail.com>
2018-03-24 18:10:24 +0800

    把字符串转换大小写-去掉空白字符

---
+++ 把字符串转换大小写-去掉空白字符.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "www.taikr.com";
+            for (int i = 0; i < str.Length;i ++ )
+            {
+                Console.WriteLine(str[i]);
+            }
+            //string res = str.ToLower();//把字符串转换成小写 并返回
+            //string res = str.ToUpper();//把字符串转换成大写 并返回
+            string res = str.Trim();//去掉前边和后边的 空白字符
+            string res1 = str.TrimStart();//去掉前边的 空白字符
+            string res2 = str.TrimEnd();//去掉后边的 空白字符
+            Console.ReadKey();
+        }
+    }
+}
